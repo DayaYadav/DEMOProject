a97403ec31d0fdf23d020b8d928cacadfc97a6fb
DayaYadav <daya20oct@gmail.com>
2025-01-24 15:21:45 +0530

    update login screen test case

--- Demo/app/src/androidTest/java/com/example/demo/View/LoginScreenKtTest.kt
+++ Demo/app/src/androidTest/java/com/example/demo/View/LoginScreenKtTest.kt
@@ -1,17 +1,23 @@
 package com.example.demo.View
 
 import androidx.activity.compose.setContent
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.isDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import androidx.navigation.testing.TestNavHostController
 import androidx.test.core.app.takeScreenshot
 import com.example.demo.Activity.MainActivity
 import com.example.demo.UtillClass.Screen
+import com.example.demo.ViewModelClass.HomeViewModel
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.delay
 import org.junit.After
 import org.junit.Assert.assertTrue
 import org.junit.Before
@@ -21,16 +27,18 @@ import org.junit.Test
 class LoginScreenKtTest {
 
     @get:Rule
-
     val composeTestRule = createAndroidComposeRule<MainActivity>()
 
     private lateinit var navController: TestNavHostController
+
+    private lateinit var viewModel: HomeViewModel
 
     @Before
     fun setUp() {
 
         // Render the Compose UI
         composeTestRule.activity.setContent {
+            var navController = rememberNavController()
             // Call the entry point composable function
             NavHost(navController = navController, startDestination = Screen.LoginScreen.route) {
 
@@ -70,12 +78,14 @@ class LoginScreenKtTest {
 
         // Find the ENTER button and perform a click
         composeTestRule.onNodeWithText("Next")
-            .performClick()
+            .isDisplayed( )
 
         // Assert that the login action is triggered with the correct credentials
         // For demonstration purposes, we'll just check that the entered email and password are non-empty.
         assertTrue(userName.isNotEmpty())
         assertTrue(password.isNotEmpty())
+
+        takeScreenshot()
     }
 
 }
---
+++ Demo/app/src/androidTest/java/com/example/demo/View/UserDetailScreenKtTest.kt
@@ -0,0 +1,61 @@
+package com.example.demo.View
+
+import androidx.activity.compose.setContent
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.testing.TestNavHostController
+import androidx.test.core.app.takeScreenshot
+import com.example.demo.Activity.MainActivity
+import com.example.demo.UtillClass.Screen
+import com.example.demo.ViewModelClass.HomeViewModel
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class UserDetailScreenKtTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+  private lateinit var navController: TestNavHostController
+
+    private lateinit var viewModel: HomeViewModel
+
+    @Before
+    fun setUp() {
+        // Render the Compose UI
+        composeTestRule.activity.setContent {
+            var navController = rememberNavController()
+            // Call the entry point composable function
+            NavHost(navController = navController, startDestination = Screen.DetailsScreen.route) {
+
+                composable(Screen.DetailsScreen.route) { UserDetailScreen("Hello",viewModel,navController) } // Your composable screen
+
+            }
+
+        }
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun userDetailScreen() {
+
+        composeTestRule.onNodeWithText("Welcome: ").assertIsDisplayed()
+        takeScreenshot()
+
+    }
+
+    @Test
+    fun customRow() {
+    }
+}
